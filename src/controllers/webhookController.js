5a99d167161ed9e804968f26bc78cfe5ce7e5e30
Aman <amank422815@gmail.com>
2026-07-27 22:37:33 +0530

    changes in popup lists of categoryItems and update messages into hinglish

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -23,7 +23,7 @@ const MAIN_MENU = {
     { id: "view_cart", title: "🛒 View Cart" },
     { id: "support", title: "💬 Support & Queries" },
   ],
-  footer: 'Type "menu" anytime',
+  footer: 'Type "menu" anytime to return here brother',
 };
 
 // Support menu configuration
