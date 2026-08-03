75fe63b493810d98deb30f8e175bfe1f817feac9
Aman <amank422815@gmail.com>
2026-08-03 16:48:58 +0530

    changes in bot logics and trying to user experience smooth

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -916,7 +916,7 @@ Rajasthan
               await sendButtonMessage(
                 from,
                 "✅ Aapka cart khali kar diya gaya hai.\n\nAgar dobara kuch order karna ho toh neeche se start kar sakte hain",
-                [{ id: "orders", title: "🛒 Start Shopping" }],
+                [{ id: "orders", title: "🏠 Main Menu" }],
               );
               await conversation.setState(from, "menu");
               continue;
